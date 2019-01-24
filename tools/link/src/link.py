e96c33f5bdd5ff2c41fe6d8673ee324259781a3d
matthias <matthias@calvin>
2019-01-24 12:00:47 +0100

    linker shot

--- tools/link/src/link.py
+++ tools/link/src/link.py
@@ -1,5 +1,32 @@
+obj_files = []
+
+
+def obj_file_exists(file_name):
+    return file_name in obj_files
+
+
+def add_obj_file(file_name):
+    if not obj_file_exists(file_name):
+        obj_files[file_name] = {
+            'symbol_table': [],
+            'symbols': {}
+        }
+
+
+def read_obj_header(obj, errors=None):
+    return True
+
+
+def read_obj_symbol_table(obj, errors=None):
+    return True
+
+
 def read_obj_file(file_name):
-    pass
+    errors = []
+
+    with open(file_name, 'rb') as obj:
+        read_obj_header(obj, errors)
+        read_obj_symbol_table(obj, errors)
 
 
 def read_obj_files(file_names):
