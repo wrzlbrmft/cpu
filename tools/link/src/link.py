3fdb61a89549d6fea168060ed7592355fc93b847
matthias <matthias@calvin>
2019-01-24 14:07:11 +0100

    error handling

--- tools/link/src/link.py
+++ tools/link/src/link.py
@@ -3,8 +3,20 @@ import struct
 total_errors_count = 0
 current_file_name = None
 current_file_errors_count = 0
+current_symbol_name = None
+current_symbol_errors_count = 0
 
 obj_files = {}
+
+
+error_messages = {
+    'DUPLICATE_OBJ_FILE': "duplicate object file '{}'",
+    'UNEXPECTED_EOF': 'unexpected end of file',
+    'INVALID_SYMBOL_TABLE_SIZE': 'invalid symbol table size',
+    'CORRUPT_SYMBOL_TABLE': 'corrupt symbol table',
+    'CORRUPT_RELOCATIONS': 'corrupt relocations',
+    'CORRUPT_MACHINE_CODE': 'corrupt machine code'
+}
 
 
 def obj_file_exists(file_name):
@@ -70,19 +82,53 @@ def read_str(file):
         return None
 
 
+def show_error(error):
+    global total_errors_count, current_file_errors_count, current_symbol_errors_count
+
+    total_errors_count += 1
+
+    if current_symbol_name and not current_symbol_errors_count:
+        current_symbol_errors_count += 1
+        if current_file_name:
+            print(f'{current_file_name}: ', end='')
+        print(f"in symbol '{current_symbol_name}':")
+
+    if current_file_name:
+        current_file_errors_count += 1
+        print(f'{current_file_name}: ', end='')
+
+    print('error:', error_messages[error['name']].format(*error['info']))
+
+    print()
+
+
 def read_obj_header(obj, errors=None):
-    return True
+    pass
 
 
 def read_obj_symbol_table(obj, errors=None):
-    errors = []
-
     symbol_table_size = read_value_little_endian(obj)
-    if symbol_table_size > 0:
+    if symbol_table_size is None:
+        errors.append({
+            'name': 'UNEXPECTED_EOF',
+            'info': []
+        })
+    elif 0 == symbol_table_size:
+        errors.append({
+            'name': 'INVALID_SYMBOL_TABLE_SIZE',
+            'info': []
+        })
+    else:
         for i in range(0, symbol_table_size):
             symbol_name = read_str(obj)
             machine_code_size = read_value_little_endian(obj)
-            if symbol_name is not None and machine_code_size is not None:
+            if symbol_name is None or machine_code_size is None:
+                errors.append({
+                    'name': 'CORRUPT_SYMBOL_TABLE',
+                    'info': []
+                })
+                break
+            else:
                 obj_file = get_obj_file(current_file_name)
                 obj_file['symbol_table'].append(symbol_name)
                 obj_file['symbols'][symbol_name] = {
@@ -90,42 +136,47 @@ def read_obj_symbol_table(obj, errors=None):
                     'machine_code': bytearray(),
                     'relocations': []
                 }
-            else:
-                pass
-    else:
-        pass
-
-    return not errors
 
 
 def read_obj_symbols(obj, errors=None):
-    errors = []
+    global current_symbol_name, current_symbol_errors_count
 
     obj_file = get_obj_file(current_file_name)
     for symbol_name, symbol in obj_file['symbols'].items():
+        current_symbol_name = symbol_name
+        current_symbol_errors_count = 0
+
         relocations_size = read_value_little_endian(obj)
-        if relocations_size is not None:
+        if relocations_size is None:
+            errors.append({
+                'name': 'UNEXPECTED_EOF',
+                'info': []
+            })
+        else:
             for i in range(0, relocations_size):
                 machine_code_offset = read_value_little_endian(obj)
                 symbol_index = read_value_little_endian(obj)
-                if machine_code_offset is not None and symbol_index is not None:
+                if machine_code_offset is None or symbol_index is None:
+                    errors.append({
+                        'name': 'CORRUPT_RELOCATIONS',
+                        'info': []
+                    })
+                    break
+                else:
                     symbol['relocations'].append({
                         'machine_code_offset': machine_code_offset,
                         'symbol_index': symbol_index
                     })
-                else:
-                    pass
 
-            if not errors:
+            if relocations_size is not None:
                 machine_code = obj.read(symbol['machine_code_size'])
                 if len(machine_code) == symbol['machine_code_size']:
                     symbol['machine_code'].extend(machine_code)
                 else:
-                    pass
-        else:
-            pass
-
-    return not errors
+                    errors.append({
+                        'name': 'CORRUPT_MACHINE_CODE',
+                        'info': []
+                    })
 
 
 def read_obj_file(file_name):
@@ -135,7 +186,10 @@ def read_obj_file(file_name):
 
     with open(file_name, 'rb') as obj:
         if obj_file_exists(file_name):
-            pass
+            show_error({
+                'name': 'DUPLICATE_OBJ_FILE',
+                'info': [file_name]
+            })
         else:
             current_file_name = file_name
             current_file_errors_count = 0
@@ -146,6 +200,9 @@ def read_obj_file(file_name):
             read_obj_symbol_table(obj, errors)
             read_obj_symbols(obj, errors)
 
+            for error in errors:
+                show_error(error)
+
 
 def read_obj_files(file_names):
     for file_name in file_names:
@@ -155,4 +212,4 @@ def read_obj_files(file_names):
 # main
 
 
-read_obj_files(['test1.obj'])
+read_obj_files(['corrupt.obj'])
