9deb2cd3ad6bd95503f60933a6f7fd21bb4d8cba
matthias <matthias@calvin>
2019-01-26 15:59:11 +0100

    better comments

--- tools/asm/src/bounce.asm
+++ tools/asm/src/bounce.asm
@@ -1,11 +1,11 @@
 main:   mov a,0
 
-up:     nop     ; print
+up:     nop     ; print(a)
         add 1
         cmp 100
         jnz up
 
-down:   nop     ; print
+down:   nop     ; print(a)
         sub 1
         jnz down
 
