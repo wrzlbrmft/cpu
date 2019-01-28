3fe33d05cdf32b147922f631a4feb5d35a9e6d78
matthias <matthias@calvin>
2019-01-28 01:28:38 +0100

    parsing

--- tools/dev/src/asm.py
+++ tools/dev/src/asm.py
@@ -1,6 +1,34 @@
+import shlex
+
 current_asm_file_name = None
 current_asm_line_num = 0
 current_asm_line_str = None
+
+
+def parse_asm_line_str(line_str, errors=None):
+    directive = None
+    symbol_name = None
+    mnemonic = None
+    operands = []
+    operand = ''
+    operand_expected = False
+
+    parser = shlex.shlex(line_str)
+    parser.commenters = ';'
+    parser.wordchars += '.:'
+
+    for token in parser:
+        pass
+
+    if errors:
+        return False
+    else:
+        return {
+            'directive': directive,
+            'symbol_name': symbol_name,
+            'mnemonic': mnemonic,
+            'operands': operands
+        }
 
 
 def assemble_asm_file(file_name):
@@ -15,6 +43,8 @@ def assemble_asm_file(file_name):
             current_asm_line_str = line_str
             line_num += 1
             current_asm_line_num = line_num
+
+            line = parse_asm_line_str(current_asm_line_str)
 
 
 if '__main__' == __name__:
