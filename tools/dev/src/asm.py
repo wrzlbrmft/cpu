f43e3cbc89e3ae495745d6076346d2f4f29ac97b
matthias <matthias@calvin>
2019-01-28 01:25:43 +0100

    parsing

--- tools/dev/src/asm.py
+++ tools/dev/src/asm.py
@@ -1,2 +1,21 @@
+current_asm_file_name = None
+current_asm_line_num = 0
+current_asm_line_str = None
+
+
+def assemble_asm_file(file_name):
+    global current_asm_file_name, current_asm_line_num, current_asm_line_str
+
+    with open(file_name, 'r') as asm:
+        current_asm_file_name = file_name
+        line_num = 0
+        current_asm_line_num = line_num
+
+        for line_str in asm.readlines():
+            current_asm_line_str = line_str
+            line_num += 1
+            current_asm_line_num = line_num
+
+
 if '__main__' == __name__:
     pass
