9f39e294675a1d04ec8acceae7e9d45f5273c8de
matthias <matthias@calvin>
2019-01-28 05:40:57 +0100

    endianness

--- tools/dev/src/endianness.py
+++ tools/dev/src/endianness.py
@@ -7,3 +7,11 @@ def word_to_le(value):
 
 def word_to_be(value):
     return struct.pack('>H', value)
+
+
+def le_to_word(values):
+    return struct.unpack('<H', values)[0]
+
+
+def be_to_word(values):
+    return struct.unpack('>H', values)[0]
