d1691522df3dfc217cd7ef6f3adb93f70602815a
matthias <matthias@calvin>
2019-01-29 16:01:31 +0100

    add, then load

--- tools/dev/src/symbols.py
+++ tools/dev/src/symbols.py
@@ -25,16 +25,13 @@ def get_symbol(symbol_name, symbols=None):
         return None
 
 
-def add_symbol(symbol_name, symbol=None, symbols=None):
+def add_symbol(symbol_name, symbols=None):
     if symbols is None:
         symbols = _symbols
 
     if not symbol_exists(symbol_name, symbols):
-        if symbol is None:
-            symbols[symbol_name] = {
-                'machine_code': bytearray(),
-                'relocation_table': [],
-            }
-        else:
-            symbols[symbol_name] = symbol
+        symbols[symbol_name] = {
+            'machine_code': bytearray(),
+            'relocation_table': [],
+        }
     return get_symbol(symbol_name, symbols)
