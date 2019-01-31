8645971ddb4a956a6f0e0455e8f3c420ac5918dd
matthias <matthias@calvin>
2019-01-31 01:21:35 +0100

    obj files

--- tools/dev/src/link.py
+++ tools/dev/src/link.py
@@ -1,5 +1,28 @@
 import sys
 
+obj_files = {}
+
+
+def obj_file_exists(file_name):
+    return file_name in obj_files
+
+
+def get_obj_file(file_name):
+    if obj_file_exists(file_name):
+        return obj_files[file_name]
+    else:
+        return None
+
+
+def add_obj_file(file_name):
+    if not obj_file_exists(file_name):
+        obj_files[file_name] = {
+            'symbol_table': [],
+            'symbols': {}
+        }
+
+    return get_obj_file(file_name)
+
 
 # main
 
