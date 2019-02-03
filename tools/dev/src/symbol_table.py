73bf341d5527a4770a0e7da8dff8306fdb4d8717
matthias <matthias@calvin>
2019-02-03 23:53:57 +0100

    typo

--- tools/dev/src/symbol_table.py
+++ tools/dev/src/symbol_table.py
@@ -1,5 +1,5 @@
 # a symbol table is an array of symbol names
-# note: relocation tables are using the array index as the reference to the symbol name
+# note: relocation tables are using the array index of the symbol name as the reference
 
 _symbol_table = []
 
