a5904f67ba6760b194acb33c9eb0e6bdeaa13dad
matthias <matthias@calvin>
2019-02-18 00:18:59 +0100

    args

--- tools/rom/src/rom.py
+++ tools/rom/src/rom.py
@@ -1,5 +1,10 @@
 import sys
 
+address_config = {}
+data_config_column = None
+data_config_bits = None
+control_signals = {}
+
 
 # main
 
@@ -9,6 +14,9 @@ def main():
         pass
     else:
         csv_file_name = sys.argv[1]
+        address_config_str = sys.argv[2]
+        data_config_str = sys.argv[3]
+        output_format = sys.argv[4]
 
 
 if '__main__' == __name__:
