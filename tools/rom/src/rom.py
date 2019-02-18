09dc6a4e7573845ae3e0b73257bb9a4bf0494321
matthias <matthias@calvin>
2019-02-18 01:04:14 +0100

    parse address config

--- tools/rom/src/rom.py
+++ tools/rom/src/rom.py
@@ -6,10 +6,33 @@ data_config_bits = None
 control_signals = {}
 
 
+def parse_address_config(config_str):
+    config = {}
+
+    for i in config_str.split(','):
+        j = i.split(':')
+
+        column = j[0]
+        if not column.isdecimal():
+            return None
+
+        bits = None
+        if len(j) > 1:
+            bits = j[1]
+            if not bits.isdecimal():
+                return None
+
+        config[column] = bits
+
+    return config
+
+
 # main
 
 
 def main():
+    global address_config
+
     if len(sys.argv) < 5:
         pass
     else:
@@ -17,6 +40,9 @@ def main():
         address_config_str = sys.argv[2]
         data_config_str = sys.argv[3]
         output_format = sys.argv[4]
+
+        address_config = parse_address_config(address_config_str)
+        print(address_config)
 
 
 if '__main__' == __name__:
