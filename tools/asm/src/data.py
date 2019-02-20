dea81f6552a35cfe8ed2d4b7968004e1f0ecbb34
matthias <matthias@calvin>
2019-02-20 01:15:46 +0100

    renamed params

--- tools/asm/src/data.py
+++ tools/asm/src/data.py
@@ -8,63 +8,63 @@ valid_chr_regex = re.compile('(\'.\'|\".\")', re.IGNORECASE)
 valid_str_regex = re.compile('(\'.{2,}\'|\".{2,}\")', re.IGNORECASE)
 
 
-def is_valid_dec(data):
-    return valid_dec_regex.fullmatch(data)
+def is_valid_dec(s):
+    return valid_dec_regex.fullmatch(s)
 
 
-def is_valid_hex(data):
-    return valid_hex_regex.fullmatch(data)
+def is_valid_hex(s):
+    return valid_hex_regex.fullmatch(s)
 
 
-def is_valid_bin(data):
-    return valid_bin_regex.fullmatch(data)
+def is_valid_bin(s):
+    return valid_bin_regex.fullmatch(s)
 
 
-def is_valid_oct(data):
-    return valid_oct_regex.fullmatch(data)
+def is_valid_oct(s):
+    return valid_oct_regex.fullmatch(s)
 
 
-def is_valid_chr(data):
-    return valid_chr_regex.fullmatch(data)
+def is_valid_chr(s):
+    return valid_chr_regex.fullmatch(s)
 
 
-def is_valid_str(data):
-    return valid_str_regex.fullmatch(data)
+def is_valid_str(s):
+    return valid_str_regex.fullmatch(s)
 
 
-def is_valid(data):
-    return '0' == data or \
-        is_valid_dec(data) or \
-        is_valid_hex(data) or \
-        is_valid_bin(data) or \
-        is_valid_oct(data) or \
-        is_valid_chr(data) or \
-        is_valid_str(data)
+def is_valid(s):
+    return '0' == s or \
+        is_valid_dec(s) or \
+        is_valid_hex(s) or \
+        is_valid_bin(s) or \
+        is_valid_oct(s) or \
+        is_valid_chr(s) or \
+        is_valid_str(s)
 
 
-def get_value(data):
-    if '0' == data:
+def get_value(s):
+    if '0' == s:
         return 0
-    elif is_valid_dec(data):
-        return int(data)
-    elif is_valid_hex(data):
-        return int(data[2:], 16)
-    elif is_valid_bin(data):
-        return int(data[2:], 2)
-    elif is_valid_oct(data):
-        return int(data[1:], 8)
-    elif is_valid_chr(data):
-        return ord(data[1])
-    elif is_valid_str(data):
+    elif is_valid_dec(s):
+        return int(s)
+    elif is_valid_hex(s):
+        return int(s[2:], 16)
+    elif is_valid_bin(s):
+        return int(s[2:], 2)
+    elif is_valid_oct(s):
+        return int(s[1:], 8)
+    elif is_valid_chr(s):
+        return ord(s[1])
+    elif is_valid_str(s):
         # string data is returned as a tuple containing the ascii codes of the individual characters
         # note: codes can be >255 because of unicode characters
-        return tuple(map(ord, data[1:-1]))
+        return tuple(map(ord, s[1:-1]))
     else:
         return None
 
 
-def get_size(data):
-    value = get_value(data)
+def get_size(s):
+    value = get_value(s)
     if isinstance(value, tuple):
         # for tuples (string data), return the size of the largest item (character)
         # note: size can be >8 bit because of unicode characters
