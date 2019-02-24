acc582cedbadabc4ad13e286d117d06d2cd95a0c
matthias <matthias@calvin>
2019-02-24 19:45:55 +0100

    review

--- tools/rom/src/i18n.py
+++ tools/rom/src/i18n.py
@@ -5,15 +5,15 @@ error_messages = {
     'INVALID_OUTPUT_BIT': "invalid output bit '{}'",
     'INVALID_COLUMN_NUMBER': "invalid column number '{}'",
     'INVALID_BIT_WIDTH': "invalid bit width '{}'",
-    'FLAGS_EXCEEDING_BIT_WIDTH': "flags exceeding bit width (flags: {}-bit, given: {}-bit)",
     'FILE_NOT_FOUND': "file not found '{}'",
     'INVALID_FLAG_VALUE': "invalid flag value '{}'",
     'INVALID_FLAG_NAME': "invalid flag name '{}'",
+    'FLAGS_EXCEEDING_BIT_WIDTH': "flags exceeding bit width (flags: {}-bit, given: {}-bit)",
     'INVALID_ADDR_VALUE': "invalid address value '{}'",
     'ADDR_COLUMN_NOT_FOUND': "address column not found '{}'",
     'INCOMPATIBLE_ADDR_SIZE': 'incompatible address size (given: {}-bit, max: {}-bit)',
+    'UNKNOWN_FLAG': "unknown flag '{}'",
     'INVALID_DATA_VALUE': "invalid data value '{}'",
     'DATA_COLUMN_NOT_FOUND': "data column not found '{}'",
-    'INCOMPATIBLE_DATA_SIZE': 'incompatible data size (given: {}-bit, max: {}-bit)',
-    'UNKNOWN_FLAG': "unknown flag '{}'"
+    'INCOMPATIBLE_DATA_SIZE': 'incompatible data size (given: {}-bit, max: {}-bit)'
 }
