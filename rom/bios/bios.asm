0f8caf564d6283bf5b4805b03c25f4dbe8eb6143
matthias <matthias@calvin>
2019-03-03 20:20:47 +0100

    bios with interrupt pointers

--- rom/bios/bios.asm
+++ rom/bios/bios.asm
@@ -1,1 +1,193 @@
-main:   mov sp, 0xffff  ; initialize stack pointer
+main:   jmp int00
+        nop
+
+_int01: jmp 0x0000
+        nop
+
+_int02: jmp 0x0000
+        nop
+
+_int03: jmp 0x0000
+        nop
+
+_int04: jmp 0x0000
+        nop
+
+_int05: jmp 0x0000
+        nop
+
+_int06: jmp 0x0000
+        nop
+
+_int07: jmp 0x0000
+        nop
+
+_int08: jmp 0x0000
+        nop
+
+_int09: jmp 0x0000
+        nop
+
+_int0a: jmp 0x0000
+        nop
+
+_int0b: jmp 0x0000
+        nop
+
+_int0c: jmp 0x0000
+        nop
+
+_int0d: jmp 0x0000
+        nop
+
+_int0e: jmp 0x0000
+        nop
+
+_int0f: jmp 0x0000
+        nop
+
+_int10: jmp 0x0000
+        nop
+
+_int11: jmp 0x0000
+        nop
+
+_int12: jmp 0x0000
+        nop
+
+_int13: jmp 0x0000
+        nop
+
+_int14: jmp 0x0000
+        nop
+
+_int15: jmp 0x0000
+        nop
+
+_int16: jmp 0x0000
+        nop
+
+_int17: jmp 0x0000
+        nop
+
+_int18: jmp 0x0000
+        nop
+
+_int19: jmp 0x0000
+        nop
+
+_int1a: jmp 0x0000
+        nop
+
+_int1b: jmp 0x0000
+        nop
+
+_int1c: jmp 0x0000
+        nop
+
+_int1d: jmp 0x0000
+        nop
+
+_int1e: jmp 0x0000
+        nop
+
+_int1f: jmp 0x0000
+        nop
+
+_int20: jmp 0x0000
+        nop
+
+_int21: jmp 0x0000
+        nop
+
+_int22: jmp 0x0000
+        nop
+
+_int23: jmp 0x0000
+        nop
+
+_int24: jmp 0x0000
+        nop
+
+_int25: jmp 0x0000
+        nop
+
+_int26: jmp 0x0000
+        nop
+
+_int27: jmp 0x0000
+        nop
+
+_int28: jmp 0x0000
+        nop
+
+_int29: jmp 0x0000
+        nop
+
+_int2a: jmp 0x0000
+        nop
+
+_int2b: jmp 0x0000
+        nop
+
+_int2c: jmp 0x0000
+        nop
+
+_int2d: jmp 0x0000
+        nop
+
+_int2e: jmp 0x0000
+        nop
+
+_int2f: jmp 0x0000
+        nop
+
+_int30: jmp 0x0000
+        nop
+
+_int31: jmp 0x0000
+        nop
+
+_int32: jmp 0x0000
+        nop
+
+_int33: jmp 0x0000
+        nop
+
+_int34: jmp 0x0000
+        nop
+
+_int35: jmp 0x0000
+        nop
+
+_int36: jmp 0x0000
+        nop
+
+_int37: jmp 0x0000
+        nop
+
+_int38: jmp 0x0000
+        nop
+
+_int39: jmp 0x0000
+        nop
+
+_int3a: jmp 0x0000
+        nop
+
+_int3b: jmp 0x0000
+        nop
+
+_int3c: jmp 0x0000
+        nop
+
+_int3d: jmp 0x0000
+        nop
+
+_int3e: jmp 0x0000
+        nop
+
+_int3f: jmp 0x0000
+        nop
+
+int00:  mov sp, 0xffff  ; initialize stack pointer
