b8efa95b87a39bd6f056e37855fab0d1e23bc162
matthias <matthias@calvin>
2019-03-05 18:42:38 +0100

    bounce using calls

---
+++ tools/asm/examples/bounce-call.asm
@@ -0,0 +1,21 @@
+main:   mov sp, 0xffff  ; initialize stack pointer
+        mov a,0
+
+up:     nop     ; print(a)
+        call inc
+        cmp 0b1111
+        jnz up
+
+down:   nop     ; print(a)
+        call dec
+        jnz down
+
+        jmp up
+
+inc:    add 1
+        ret
+
+dec:    sub 1
+        ret
+
+.end
